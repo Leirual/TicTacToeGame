d7fa78eb02de01c73c03f27474909826e8604b53
Paweł Apanasewicz <pawel.apanasewicz@codeme.pl>
2016-02-08 16:26:02 +0100

    Opisany program z zajęć

--- src/main/java/pl/codeme/jse1/gra/App.java
+++ src/main/java/pl/codeme/jse1/gra/App.java
@@ -1,39 +1,66 @@
-package pl.codeme.jse1.gra;
+package pl.codeme.jse1.gra; // nazwa pakietu
 
-import java.util.Scanner;
+import java.util.Scanner; // import klasy używanej w programie
 
-public class App {
+/**
+ * Klasa uruchamiająca aplikację
+ * 
+ * @author pawel.apanasewiczcodeme.pl
+ *
+ */
+public class App { // deklaracja klasy
+    /**
+     * Tablica literałów cyfr
+     */
+    static String[] tab = {"zero", "jeden", "dwa", "trzy", "cztery", "pięć", "ala", "siedem"};
 
-    public static void main(String[] args) {
-        System.out.println("Podaj liczby z przedziału 0 - 5");
-        Scanner scanner = new Scanner(System.in);
-        int liczba = Integer.valueOf(scanner.nextLine());
+    /**
+     * Funkcja zamieniająca cyfrę na odpowiadający jej literał
+     * 
+     * @param liczba Podana cyfra
+     * 
+     * @return Literał
+     */
+    static String zamien(int liczba) {
         String tekst;
-        switch(liczba) {
-            case 0:
-                tekst = "zero";
-                break;
-            case 1:
-                tekst = "jeden";
-                break;
-            case 2:
-                tekst = "dwa";
-                break;
-            case 3:
-                tekst = "trzy";
-                break;
-            case 4:
-                tekst = "cztery";
-                break;
-            case 5:
-                tekst = "pięć";
-                break;
-            default:
-                tekst = "podaj odpowiednia liczbe";
+        if(tab.length - 1 >= liczba) {
+            tekst = tab[liczba]; // użycie indeksu tablicy do zamiany cyfry na literał
+        } else {
+            tekst = "podaj odpowiednia liczbe";
         }
-        System.out.println("ze switch " + tekst);
 
-        scanner.close();
+        return tekst;
+    }
+
+    /**
+     * Metoda uruchamialna
+     * 
+     * @param args Parametry z linii komend
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // inicjalizacja obiektu komunikującego się z konsolą
+
+        int liczba = 0;
+        // nieskończona pętla aplikacji
+        do {
+            System.out.println("Podaj liczby z przedziału 0 - " + (tab.length - 1));
+            String komenda = scanner.nextLine(); // pobranie tekstu z konsoli
+
+            // jeśli podany tekst to end przerywamy działanie pętli
+            if(komenda.equals("end")) {
+                break;
+            }
+
+           liczba = Integer.valueOf(komenda); // konwersja tekstu na liczbę
+            System.out.println("ze switch " + zamien(liczba));
+        } while(true);
+
+        // pętla for postać do obsługi kolekcji
+        for(String item : tab) {
+            System.out.println(item);
+        }
+
+        scanner.close(); // zamknięcie połączenia z konsolą
     }
 
 }
