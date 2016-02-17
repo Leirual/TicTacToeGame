3c8f81f55a9d0e17a1b16a818e4e1c20fa19602f
Paweł Apanasewicz <pawel.apanasewicz@codeme.pl>
2016-02-17 16:44:24 +0100

    Opisany kod z warsztatow 4

--- src/main/java/pl/codeme/jse1/oop/Miejsce.java
+++ src/main/java/pl/codeme/jse1/oop/Miejsce.java
@@ -1,22 +1,45 @@
 package pl.codeme.jse1.oop;
 
+/**
+ * Typ wyliczeniowy miejsc siedzących
+ * 
+ * @author pawel.apanasewicz@codeme.pl
+ *
+ */
 public enum Miejsce {
-    MIEJSCE1, MIEJSCE2(true), MIEJSCE3, MIEJSCE4(true);
+    MIEJSCE1, MIEJSCE2(true), MIEJSCE3, MIEJSCE4(true); // lista elementów
 
+    /**
+     * Czy miejsce jest wolne
+     */
     private boolean wolne;
 
     Miejsce() {
         
     }
 
+    /**
+     * Konstruktor elementów z listy
+     * 
+     * @param wolne Czy wolne miejsce
+     */
     Miejsce(boolean wolne) {
         this.wolne = wolne;
     }
 
+    /**
+     * Metoda zwraca czy wolne miejsce
+     * 
+     * @return Czy wolne miejsce
+     */
     public boolean czyWolne() {
         return wolne;
     }
 
+    /**
+     * Metoda zajmuje miejsce
+     * 
+     */
     public void zajmij() {
         wolne = false;
     }
--- src/main/java/pl/codeme/jse1/oop/Samolot.java
+++ src/main/java/pl/codeme/jse1/oop/Samolot.java
@@ -5,14 +5,27 @@ package pl.codeme.jse1.oop;
 import java.util.ArrayList;
 import java.util.List;
 
-//import static pl.codeme.jse1.oop.Miejsce.MIEJSCE1;
-
+/**
+ * Klasa reprezentująca samolot
+ * 
+ * @author pawel.apanasewicz@codeme.pl
+ *
+ */
 public final class Samolot {
 
+    /**
+     * Pole opisujące lot samolotu
+     */
     public String lot = "Polska Warszawa - Wielka Brytania Londyn";
 
+    /**
+     * Metoda wyszukująca wolne miejsca w samolocie
+     * 
+     * @return Lista wolnych miejsc
+     */
     public List<Miejsce> znajdzWolneMiejsce() {
-        List<Miejsce> listaWolnychMiejsc = new ArrayList<>();
+        List<Miejsce> listaWolnychMiejsc = new ArrayList<>(); // deklaracja i definicja List (kolekcji)
+        // przekjrzenie i dodanie do listy wszystkich wolnych miejsc
         for(Miejsce miejsce : Miejsce.values()) {
             if(miejsce.czyWolne()) {
                 listaWolnychMiejsc.add(miejsce);
@@ -21,20 +34,25 @@ public final class Samolot {
         return listaWolnychMiejsc;
     }
 
+    /**
+     * Metoda uruchamialna
+     * 
+     * @param args Parametry linii komend
+     */
     public static void main(String[] args) {
         Samolot samolot = new Samolot();
-        List<Miejsce> listaWolnychMiejsc = samolot.znajdzWolneMiejsce();
-        listaWolnychMiejsc.get(1).zajmij();
+        List<Miejsce> listaWolnychMiejsc = samolot.znajdzWolneMiejsce(); // pobranie wolnych miejsc
+        listaWolnychMiejsc.get(1).zajmij(); // zajęcie piejsca z pozycji 2
         System.out.println(samolot.znajdzWolneMiejsce());
 
-        String[] strTab = samolot.lot.split(" ");
+        String[] strTab = samolot.lot.split(" "); // zamiana opisu lotu na tablice po " "
         for(String item : strTab) {
             System.out.println(item);
         }
 
         String slowo = "Londyn";
-        int poz = samolot.lot.toLowerCase().indexOf(slowo.toLowerCase());
-        String zm = samolot.lot.toLowerCase().substring(poz, poz + slowo.length());
+        int poz = samolot.lot.toLowerCase().indexOf(slowo.toLowerCase()); // wyszukiwanie pozycji slowo
+        String zm = samolot.lot.toLowerCase().substring(poz, poz + slowo.length()); // wycięcie slowo
         System.out.println(zm);
     }
 
